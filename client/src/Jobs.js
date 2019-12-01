b396fc18247614370b4f59ba9906c22b575c7be1
ch264 <christinavhastenrath@gmail.com>
2019-12-01 13:08:55 -0800

    setup app and create first component

---
+++ client/src/Jobs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+// component holds styling
+
+export default function Jobs() {
+	return (
+		<div>
+
+		</div>
+	)
+}
