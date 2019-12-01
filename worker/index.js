c74dc72d31b868364ed6e280fb259eae62290232
ch264 <christinavhastenrath@gmail.com>
2019-12-01 14:23:33 -0800

    schedule job to call api

--- worker/index.js
+++ worker/index.js
@@ -1,9 +1,8 @@
 // manages worker tasks 
 
+const fetchGithub = require('./tasks/fetch-github')
 
 // run cron jobs by running node process
 // to test run  node worker/index.js
 var CronJob = require('cron').CronJob;
-new CronJob('* * * * * *', function() {
-  console.log('You will see this message every second');
-}, null, true, 'America/Los_Angeles');
+new CronJob('*/1 * * * * *', fetchGithub, null, true, 'America/Los_Angeles');
